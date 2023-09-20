5643b2e9e142555d8669477bc6d831e72e9a4566
portalpap <colionelson@gmail.com>
2023-09-20 15:01:21 -0600

    minimize

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
 
 const packageJson = require("./package.json");
 
@@ -22,6 +24,7 @@ export default [
       },
     ],
     plugins: [
+      PeerDepsExternalPlugin(),
       resolve(),
       commonjs(),
       typescript({
@@ -29,6 +32,7 @@ export default [
         exclude: ["**/*.stories.tsx", "**/*.test.tsx"],
       }),
       postcss(),
+      terser(),
     ],
   },
   {
